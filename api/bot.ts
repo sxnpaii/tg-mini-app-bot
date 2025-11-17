4edfc68f15edc0184ed5df3663e8386475e0610e
Xurshid Abdulxayev <xurshid.abdulxayev@haulerhub.com>
2025-11-17 17:32:52 +0500

    feat: separated arch. new commands

--- api/bot.ts
+++ api/bot.ts
@@ -1,23 +1,11 @@
 import { Bot, InlineKeyboard, Keyboard, webhookCallback } from "grammy";
-
+import { commands } from "./commands";
 // ! ALWAYS CHECKOUT BEFORE DEPLOY
 const botToken = process.env.BOT_TOKEN_PROD || "";
-const miniAppUrl = process.env.WEB_APP_PROD || "";
+const botUrl = process.env.BOT_URL_PROD || "";
+
 const bot = new Bot(botToken);
 
-bot.command("start", async (ctx) => {
-  await ctx.reply(
-    `Welcome to the HaulerHub bot!\n
-To use bot, you need to launch Mini App
-    `,
-    {
-      reply_markup: new InlineKeyboard().webApp(
-        "Login to Platform",
-        miniAppUrl
-      ),
-    }
-  );
-});
 bot.on(":location", async (ctx) => {
   const loc = ctx.message?.location;
   if (loc && loc.live_period) {
@@ -33,14 +21,25 @@ bot.on("edit:location", async (ctx) => {
   lat: ${loc?.latitude}\n
   long: ${loc?.longitude}
   `);
+
+    //
   }
 });
-bot.on(":web_app_data", (ctx) => {
+bot.on(":web_app_data", async (ctx) => {
   const web_app_data = ctx.message?.web_app_data.data;
-  ctx.reply(`Please send us live location:\n\n ${web_app_data}`);
+  ctx.reply(`Please send us live location:\n`, {
+    reply_markup: new Keyboard().requestLocation("Send Location"),
+  });
 });
+
+//composers
+bot.use(commands);
 
 // * switch on prod
 // start
 // bot.start();
+
+bot.api.setWebhook(botUrl);
 export default webhookCallback(bot, "https");
+
+export { bot };
